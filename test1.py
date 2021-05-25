e816903a3334f3a3812d248e151e1a995a96c9c6
Yuanty378 <2654429963@qq.com>
2021-05-25 16:38:07 +0800

    第一次向github提交代码

---
+++ test1.py
@@ -0,0 +1,15 @@
+#message = "hello word!";
+#print(message)
+"""
+a = int(input("请输入: "))
+b = int(input("请输入: "))
+print("input获取的内容: ",a+b)
+"""
+"""
+练习：通过代码获取两段内容，并且计算他们长度的和。
+"""
+a = input("请输入: ")
+b = input("请输入: ")
+c = len(a)
+d = len(b)
+print("两段字符串的长度和是: ",c+d)
